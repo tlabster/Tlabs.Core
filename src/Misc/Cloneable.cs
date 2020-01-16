846299ba7fba89391d313848e9231e7de4bd8c9e
Christian Jäckel <Christian Jckel@C-DELLE.POL>
2020-01-16 17:22:41 +0100

    Cloneable<T>

---
+++ src/Misc/Cloneable.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Tlabs.Misc {
+
+  ///<summary>Supports cloning, which creates a new instance of a class with the same value</summary>
+  public interface ICloneable<T> {
+    ///<summary>Create a clone of this instance.</summary>
+    T Clone();
+  }
+
+}
