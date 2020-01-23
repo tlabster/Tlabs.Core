053e3fe7f638cc523e6168c2ecc371fcc630836e
Christian Jäckel <Christian Jckel@C-DELLE.POL>
2020-01-23 16:34:20 +0100

    AwaitResult

---
+++ src/Misc/TaskAwaitExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tlabs.Misc {
+
+  ///<summary>Task await with timeout extension methods.</summary>
+  public static class TaskAwaiterExtension {
+
+    ///<summary>Await async result with <paramref name="timeout"/> milliseconds.</summary>
+    ///<exception cref="TimeoutException">thrown when task was not completed within <paramref name="timeout"/> milliseconds.</exception>
+    public static T AwaitResult<T>(this Task<T> tsk, int timeout)
+    => AwaitResultAsync(tsk, timeout).GetAwaiter().GetResult();
+
+    ///<summary>Await async result with <paramref name="timeout"/> milliseconds.</summary>
+    ///<returns>Completed task.</returns>
+    ///<exception cref="TimeoutException">thrown when task was not completed within <paramref name="timeout"/> milliseconds.</exception>
+    public static async Task<T> AwaitResultAsync<T>(this Task<T> tsk, int timeout) {
+      using (var ctokSrc = new CancellationTokenSource()) {
+        if (tsk != await Task.WhenAny(tsk, Task.Delay(timeout, ctokSrc.Token)))
+          throw new TimeoutException();
+
+        ctokSrc.Cancel();
+      }
+      return await tsk;  // Very important in order to propagate exceptions
+    }
+
+  }
+
+
+}
