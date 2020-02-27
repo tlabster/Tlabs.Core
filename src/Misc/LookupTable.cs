e6bd435fcab78fabdff4474ba67dbb35bbd97c42
Christian Jäckel <Christian Jckel@C-DELLE.POL>
2020-02-27 12:29:28 +0100

    MuxTicket

---
+++ src/Misc/LookupTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tlabs.Misc {
+
+  ///<summary>Object look-up table.</summary>
+  public class LookupTable<K, T> : IReadOnlyDictionary<K, T> {
+    ///<summary>Look-up table.</summary>
+    protected Dictionary<K, T> table; 
+    private Func<K, T> create;
+
+    ///<summary>Default ctor.</summary>
+    public LookupTable() { this.table= new Dictionary<K, T>(); }
+    ///<summary>Ctor from <paramref name="create"/> delegate.</summary>
+    public LookupTable(Func<K, T> create) : this() {
+      this.create= create;
+    }
+    ///<summary>Ctor from <paramref name="entries"/> and <paramref name="create"/> delegate.</summary>
+    public LookupTable(IEnumerable<KeyValuePair<K, T>> entries, Func<K, T> create) {
+      this.table= new Dictionary<K, T>(entries);
+      this.create= create;
+    }
+
+    ///<inheritdoc/>
+    public T this[K key] {
+      get {
+        T val;
+        if (table.TryGetValue(key, out val)) return val;
+        if (null == create) throw new KeyNotFoundException(key.ToString());
+        return table[key]= create(key);
+      }
+      set => table[key]= value;
+    }
+
+    ///<inheritdoc/>
+    public IEnumerable<K> Keys => table.Keys;
+
+    ///<inheritdoc/>
+    public IEnumerable<T> Values => table.Values;
+
+    ///<inheritdoc/>
+    public int Count => table.Count;
+
+    ///<inheritdoc/>
+    public bool ContainsKey(K key) => table.ContainsKey(key);
+
+    ///<inheritdoc/>
+    public IEnumerator<KeyValuePair<K, T>> GetEnumerator() => table.GetEnumerator();
+
+    ///<inheritdoc/>
+    public bool TryGetValue(K key, out T value) => table.TryGetValue(key, out value);
+
+    ///<inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator() => table.GetEnumerator();
+  }
+}
---
+++ src/Sync/MuxTicket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace Tlabs.Sync {
+
+  /// <summary>Key of <typeparamref name="T"/> based mutex ticket.</summary>
+  public sealed class MuxTicket<T> : IDisposable {
+    static readonly SyncTable<T> syncMap= new SyncTable<T>(k => new object());
+    /// <summary>Ctor from <paramref name="key"/> and optional <paramref name="timeOut"/>.</summary>
+    public MuxTicket(T key, int timeOut = System.Threading.Timeout.Infinite) {
+      object sync, acquiredSync= null;
+      
+      while (true) {
+        /* Retry until sync was acquied and placed in syncMap:
+         */
+        lock (syncMap) sync= syncMap[Key];
+        if (acquiredSync == sync) return;
+        if (null != acquiredSync && Monitor.IsEntered(acquiredSync)) Monitor.Exit(acquiredSync); //not from syncMap: release
+        if (!Monitor.TryEnter(sync, timeOut)) throw new TimeoutException($"Failed to accquire {nameof(MuxTicket<T>)} within {timeOut} msec.");
+        acquiredSync= sync;
+      }
+    }
+
+    /// <summary>Dtor.</summary>
+    ~MuxTicket() => Dispose();
+
+    /// <summary>Ticket key.</summary>
+    public T Key { get; set; }
+
+    /// <summary>Release acquirement (and signal next waiter).</summary>
+    public void Dispose() {
+      lock (syncMap) {
+        var sync= syncMap.Evict(Key);
+        if (null == sync || !Monitor.IsEntered(sync)) return;
+        Monitor.Exit(sync);
+      }
+      GC.SuppressFinalize(this);
+    }
+
+    private class SyncTable<K> : Misc.LookupTable<K, object> {
+      public SyncTable(Func<K, object> create) : base(create) { }
+      public object Evict(K key) {
+        object sync;
+        if (table.TryGetValue(key, out sync))
+          table.Remove(key);
+        return sync;
+      }
+    }
+
+  }
+
+}
