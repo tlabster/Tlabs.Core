1f20545da6a8de45aa7fd96814a993545f6135ed
christian_jaeckel <c.jaeckel@tomorrowlabs.io>
2023-01-03 18:30:55 +0100

    AsyncExtension and BufferedActionRunner

---
+++ src/Sync/AsyncExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tlabs.Sync {
+
+  ///<summary>Async (task) extension utils</summary>
+  public static class AsyncExtensions {
+    ///<summary>Specify a <paramref name="timeout"/> on <paramref name="task"/> with optional <paramref name="cts"/></summary>
+    public static async Task<T> Timeout<T>(this Task<T> task, TimeSpan timeout, CancellationTokenSource cts= null) {
+      if (task == await Task.WhenAny(task, Task.Delay(timeout)))
+        return await task;
+
+      cts?.Cancel();
+      throw new TimeoutException($"{timeout} ms timeout before task completion expired.");
+    }
+  }
+}
---
+++ src/Sync/BufferedActionRunner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tlabs.Sync {
+
+  ///<summary>Buffered action runner</summary>
+  public class BufferedActionRunner : IDisposable {
+    ActionBuffer actBuf;
+
+    ///<summary>Run <paramref name="action"/> after <paramref name="bufferTime"/> has been elapsed.</summary>
+    ///<remarks>Repeated invocation before <paramref name="bufferTime"/> eleapsed are resetting the the timer...</remarks>
+    public void Run(int bufferTime, Action action) {
+      if (0 == bufferTime) {
+        action(); //run action immediately
+        return;
+      }
+
+      var newBuf= new ActionBuffer(bufferTime);
+      while (true) {
+        var oldBuf= Interlocked.CompareExchange<ActionBuffer>(ref this.actBuf, newBuf, null);
+        if (null == oldBuf) {
+          newBuf.Schedule(action);
+          return;
+        }
+        if (oldBuf == Interlocked.CompareExchange<ActionBuffer>(ref this.actBuf, null, oldBuf))
+          oldBuf.Dispose();
+      }
+    }
+
+    ///<inheritdoc/>
+    public void Dispose() {
+      actBuf?.Dispose();
+      this.actBuf= null;
+    }
+
+    private class ActionBuffer : IDisposable {
+      int delay;
+      CancellationTokenSource cts;
+      public ActionBuffer(int bufferTime) {
+        this.cts= new();
+        this.delay= bufferTime;
+      }
+
+      public void Schedule(Action action) {
+        Task.Delay(this.delay, this.cts.Token)
+            .ContinueWith(t => action(), TaskContinuationOptions.NotOnCanceled);
+      }
+
+      public void Dispose() {
+        if (null == cts) return;
+        cts.Cancel();
+        cts.Dispose();
+        cts= null;
+      }
+    }
+  }
+}
